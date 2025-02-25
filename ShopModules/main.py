34dff8279ab5525cd4c8c39612034b88f3df6041
MehdiSamamiKojidi1997 <mehdi.samami.kojidi@gmail.com>
2025-02-25 11:26:55 +0330

    Add files via upload

--- ShopModules/main.py
+++ ShopModules/main.py
@@ -1,18 +1,18 @@
 import re
 
 
-class user_management:
+class usermanagement:
     def __init__(self):
         pass#این قسمت را برای تشخیص کلید گذاشتم. اگر از قبل کلید داشته باشد نیازی
          #  به login یا signup نیست.
     def read_database(self,user_name):
         pass# این قسمت به database متصل است. خروجی در اینجا اطلاعات کاربر است.
-    def login(self,user_name,password):#خروجی این متد یک پیام و یک true یا false است.
+    def log_in(self,user_name,password):#خروجی این متد یک پیام و یک true یا false است.
         if len(self.read_database(user_name))==0 or (self.read_database(user_name))[1]!=password:
             return "Wrong Username or Password!\nDon't have an Account? Signup first!",False
         else:          #پیام برای نمایش مشکل و true یا false برای عملیات بعدی(ورود به پنجره ی جدید و...) است.
             return f"Wellcome {user_name}!",True
-    def signup(self,user_name,password,password_confirmation):
+    def sign_up(self,user_name,password,password_confirmation):
         if len(self.read_database(user_name))!=0:
             a="Username has been used before!"
             b=False
